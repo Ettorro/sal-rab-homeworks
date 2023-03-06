7464d21ac165548444b5796cf7b597f72e2db40f
Ettorro <kurmanov19851803@gmail.com>
2023-03-06 23:10:01 +0500

    Init commit

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,32 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {
+        client: "Иван +7(987)65-43-210",
+        order: {
+            address: 'ул. Академика Королёва, дом 12, 1 подъезд, 15 этаж, кв. 253',
+            sum: 900
+        },
+        goods: [
+            {
+            title: 'Пицца',
+            count: 2
+            }
+        ]
+    };
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name + ' ' + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
